7d372625e4bccee9112f254db92958545415410c
Shyam Kumar <shyam.gumudavelly@gmail.com>
2019-06-20 17:16:01 -0700

    fix

--- src/store.js
+++ src/store.js
@@ -3,7 +3,4 @@ import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-export const store = createStoreWithMiddleware(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStoreWithMiddleware(reducers);
